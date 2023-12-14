dfd5c9dea8f3aca324709b755297930dfd4ef2cd
pkm0707 <mpraveenkumar@student.tce.edu>
2023-12-14 09:25:49 +0530

    final submit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 export function Home(){
     return(
         <div>
-            <iframe  width="1536" height="500" src="https://www.youtube.com/embed/V6QqIPB6p-4?si=CxrEwgwUzI7ad6kh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <q style={{margin:110}}><i>Libraries store the energy that fuels the imagination. They open up windows to the world and inspire us to explore and achieve, and contribute to improving our quality of life.</i></q><br/><br/>
+            <iframe  style={{padding:10}} width="1515" height="500" src="https://www.youtube.com/embed/V6QqIPB6p-4?si=CxrEwgwUzI7ad6kh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
